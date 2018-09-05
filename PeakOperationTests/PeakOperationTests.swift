6480b925ba7d51c456665c77dc791618e87d0f94
Sam Oakley <sam.oakley@3squared.com>
2018-09-05 15:33:27 +0100

    Make the combination of progress in a group an optional setting

--- PeakOperationTests/PeakOperationTests.swift
+++ PeakOperationTests/PeakOperationTests.swift
@@ -306,6 +306,39 @@ class PeakOperationTests: XCTestCase {
         waitForExpectations(timeout: 10)
         
         XCTAssertEqual(progress.fractionCompleted, 1)
+        XCTAssertEqual(progress.totalUnitCount, 1)
+        print("Total Progress: \(progress.localizedAdditionalDescription!)")
+    }
+    
+    func testGroupOperationCollatingProgress() {
+        let expect = expectation(description: "")
+        
+        let firstOperation = BlockResultOperation {
+            return "Hello"
+        }
+        
+        let secondOperation = MapOperation<String, String> { input in
+            return Result { try "\(input.resolve()) World!" }
+        }
+        
+        let group1 = firstOperation
+            .passesResult(to: secondOperation)
+            .group(collateProgress: true)
+        
+        group1.addResultBlock { result in
+            do {
+                let _ = try result.resolve()
+                expect.fulfill()
+            } catch {
+                XCTFail()
+            }
+        }
+        
+        let progress = group1.enqueueWithProgress()
+        
+        waitForExpectations(timeout: 10)
+        
+        XCTAssertEqual(progress.fractionCompleted, 1)
         XCTAssertEqual(progress.totalUnitCount, 2)
         print("Total Progress: \(progress.localizedAdditionalDescription!)")
     }
