68de5d201a75f3e944b99d79f87f9a10a232c4eb
Zack Brown <zack.brown@velociti-solutions.com>
2025-09-28 14:56:03 +0100

    Update package tools version and add macOS support

--- Package.swift
+++ Package.swift
@@ -1,27 +1,19 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PeakOperation",
-    platforms: [
-        .iOS(.v11)
-    ],
+    platforms: [.macOS(.v14),
+                .iOS(.v12)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "PeakOperation",
-            targets: ["PeakOperation"]),
+        .library(name: "PeakOperation",
+                 targets: ["PeakOperation"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "PeakOperation",
-            dependencies: []),
-        .testTarget(
-            name: "PeakOperationTests",
-            dependencies: ["PeakOperation"]),
+        .target(name: "PeakOperation",),
+        .testTarget(name: "PeakOperationTests",
+                    dependencies: ["PeakOperation"]),
     ]
 )
